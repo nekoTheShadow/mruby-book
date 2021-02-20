d946f29265319cda931303b4ab4c7cb71118f7e0
hajime nakamura <h.nakamura0903@gmail.com>
2021-02-20 15:39:08 +0900

    Add FirstC::my_stat_size Test

--- projects/mruby-first_c/test/mrb_firstc.rb
+++ projects/mruby-first_c/test/mrb_firstc.rb
@@ -2,3 +2,7 @@ assert("FirstC#my_uname") do
   assert_equal(`uname -n`.chomp, FirstC.my_uname)
 end
 
+assert("FirstC#my_stat_size") do
+  assert_equal(`stat --printf "%s" /etc/hosts`.to_i, FirstC.my_stat_size("/etc/hosts"))
+  assert_raise(Errno::ENOENT) { FirstC.my_stat_size("/path/to/not_found") }
+end
