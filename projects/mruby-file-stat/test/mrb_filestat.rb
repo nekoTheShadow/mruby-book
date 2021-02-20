a268bf6aff1ecd25beb5aeaafdb412afbee68132
hajime nakamura <h.nakamura0903@gmail.com>
2021-02-21 00:21:59 +0900

    Chapter06で実装したFile::Statメソッドのテストを追加

--- projects/mruby-file-stat/test/mrb_filestat.rb
+++ projects/mruby-file-stat/test/mrb_filestat.rb
@@ -1,17 +1,22 @@
-##
-## FileStat Test
-##
-
-assert("FileStat#hello") do
-  t = FileStat.new "hello"
-  assert_equal("hello", t.hello)
+def check(method_name, path, ruby_method_name: nil, memo: nil)
+  msg = memo.nil? ? "File::Stat##{method_name}" : "File::Stat##{method_name} (#{memo})"
+  assert(msg) do 
+    expected = `ruby -e 'puts File::Stat.new("#{path}").#{ruby_method_name || method_name}'`.chomp
+    actual = File::Stat.new(path).__send__(method_name).to_s
+    assert_equal(expected, actual)
+  end
 end
 
-assert("FileStat#bye") do
-  t = FileStat.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("FileStat.hi") do
-  assert_equal("hi!!", FileStat.hi)
-end
+check("size", "/etc/hosts")
+check("inode", "/etc/hosts", ruby_method_name: "ino")
+check("uid", "/etc/hosts")
+check("gid", "/etc/hosts")
+check("dev_major", "/etc/hosts")
+check("dev_minor", "/etc/hosts")
+check("rdev_major", "/dev/null")
+check("rdev_minor", "/dev/null")
+check("file?", "/etc/hosts", memo: "return true if target is a regular file.")
+check("file?", "/etc", memo: "return false if target is not a regular file.")
+check("directory?", "/etc/hosts", memo: "return false if target is not a directory.")
+check("directory?", "/etc", memo: "return true if target is a directory.")
+check("mtime", "/etc/hosts")
