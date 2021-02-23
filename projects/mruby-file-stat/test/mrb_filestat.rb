7b13d830aed5d264bcb46f5f9e445f830ec2aa1f
hajime nakamura <h.nakamura0903@gmail.com>
2021-02-23 11:39:50 +0900

    テストの動的な生成をやめる

--- projects/mruby-file-stat/test/mrb_filestat.rb
+++ projects/mruby-file-stat/test/mrb_filestat.rb
@@ -1,28 +1,52 @@
-def check(method_name, path, ruby_method_name: nil, memo: nil)
-  msg = memo.nil? ? "File::Stat##{method_name}" : "File::Stat##{method_name} (#{memo})"
-  assert(msg) do 
-    expected = `ruby -e 'puts File::Stat.new("#{path}").#{ruby_method_name || method_name}'`.chomp
-    actual = File::Stat.new(path).__send__(method_name).to_s
-    assert_equal(expected, actual)
-  end
+assert("File::Stat#size") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").size'`.to_i, File::Stat.new("/etc/hosts").size)
 end
 
-check("size", "/etc/hosts")
-check("inode", "/etc/hosts", ruby_method_name: "ino")
-check("uid", "/etc/hosts")
-check("gid", "/etc/hosts")
-check("dev_major", "/etc/hosts")
-check("dev_minor", "/etc/hosts")
-check("rdev_major", "/dev/null")
-check("rdev_minor", "/dev/null")
-check("file?", "/etc/hosts", memo: "return true if target is a regular file.")
-check("file?", "/etc", memo: "return false if target is not a regular file.")
-check("directory?", "/etc/hosts", memo: "return false if target is not a directory.")
-check("directory?", "/etc", memo: "return true if target is a directory.")
-check("mtime", "/etc/hosts")
+assert("File::Stat#inode") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").ino'`.to_i, File::Stat.new("/etc/hosts").inode)
+end
 
-# RubyのFile::Stat#symlynkは必ずfalseになるらしいので、
-# checkメソッドではテストできない。
+assert("File::Stat#uid") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").uid'`.to_i, File::Stat.new("/etc/hosts").uid)
+end
+
+assert("File::Stat#gid") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").gid'`.to_i, File::Stat.new("/etc/hosts").gid)
+end
+
+assert("File::Stat#dev_major") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").dev_major'`.to_i, File::Stat.new("/etc/hosts").dev_major)
+end
+
+assert("File::Stat#dev_minor") do
+  assert_equal(`ruby -e 'puts File.stat("/etc/hosts").dev_minor'`.to_i, File::Stat.new("/etc/hosts").dev_minor)
+end
+
+assert("File::Stat#rdev_major") do
+  assert_equal(`ruby -e 'puts File.stat("/dev/null").rdev_major'`.to_i, File::Stat.new("/dev/null").rdev_major)
+end
+
+assert("File::Stat#rdev_minor") do
+  assert_equal(`ruby -e 'puts File.stat("/dev/null").rdev_minor'`.to_i, File::Stat.new("/dev/null").rdev_minor)
+end
+
+# mrubyのtimeはequalが実装されていないっぽいので、数値にして無理やり比較する
+assert("File::Stat#mtime") do
+  expected = `ruby -e 'puts File.stat("/dev/null").mtime.to_i'`.chomp.to_i
+  assert_equal(expected, File::Stat.new("/dev/null").mtime.to_i)
+end
+
+assert("File::Stat#file?") do
+  assert_equal(true, File::Stat.new("/etc/hosts").file?)
+  assert_equal(false, File::Stat.new("/etc").file?)
+end
+
+assert("File::Stat#directory?") do
+  assert_equal(false, File::Stat.new("/etc/hosts").directory?)
+  assert_equal(true, File::Stat.new("/etc").directory?)
+end
+
+# RubyのFile::Stat#symlynkは必ずfalseになるらしいので、Rubyを呼び出しての比較は無理。
 assert("File::Stat#symlink?") do
   assert_equal(true, File::Stat.new("/bin/vim").symlink?)
   assert_equal(false, File::Stat.new("/dev/null").symlink?)
